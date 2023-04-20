129ac4bf450815ae439757cdf29079bc671ade7f
rizkiaulia <auliarizki0970@gmail.com>
2023-04-20 09:43:34 +0700

    Tugas-PHP

--- Tugas - PHP/php-looping2.php
+++ Tugas - PHP/php-looping2.php
@@ -1,8 +1,8 @@
 <?php
-    for ($i=9; $i >= 1 ; $i--) { 
-        for ($j=1; $j <= $i ; $j++) { 
-             echo "*";
+    for ($i=9; $i >= 1 ; $i--) { //inisialisasi adalah 9, loop dimulai dari nilai i yaitu 9 kemudian melakukan pengecekan dengan kondisi apakah nilai i >= 1, jika kondisi benar maka akan menjalankan inner loop atau for yang berada didalam , kemudian terjadi proses decrement.
+        for ($j=1; $j <= $i ; $j++) { //inisialisasi adalah 1, loop dimulai dari j yaitu 1 kemudian melakukan pengecekan dengan kondisi apakah nilai j lebih kecil dari nilai i pada outer loop, jika kondisi benar makan akan mencetak nilai j, kemudian increment dan looping kembali smpai memenuhi kondisi akhir j <= i 
+             echo "*"; //mencetak bintang
         }
-        echo "<br>";
+        echo "<br>";//mencetak baris
     }
 ?>
--- Tugas - PHP/php-looping3.php
+++ Tugas - PHP/php-looping3.php
@@ -1,8 +1,8 @@
 <?php
-    for ($i=0; $i < 9 ; $i++) { 
-        for ($j=0; $j <= $i ; $j++) { 
-            echo "$j";
+    for ($i=0; $i < 9 ; $i++) { //inisialisasi adalah 0, loop dimulai dari nilai i yaitu 0 kemudian melakukan pengecekan dengan kondisi apakah nilai i < 9, jika kondisi benar maka akan menjalankan inner loop atau for yang berada didalam , kemudian terjadi proses increment.
+        for ($j=0; $j <= $i ; $j++) { //inisialisasi adalah 0, loop dimulai dari j yaitu 0 kemudian melakukan pengecekan dengan kondisi apakah nilai j lebih kecil dari nilai i pada outer loop, jika kondisi benar makan akan mencetak nilai j, kemudian increment dan looping kembali smpai memenuhi kondisi akhir j <= i 
+            echo "$j";//mencetak nilai j
         }
-        echo "<br>";
+        echo "<br>"; //mencetak baris
     }
 ?>
