f69b62f952f2b8d5262dac63afdf717222f2957d
rizkiaulia <auliarizki0970@gmail.com>
2023-04-20 09:26:14 +0700

    Tugas-PHP

---
+++ Tugas - PHP/bangun_ruang.php
@@ -0,0 +1,15 @@
+<?php
+    //1. Volume Balok 
+    $panjang = 8;
+    $lebar = 4;
+    $tinggi = 3;
+    echo "Volume Balok ( V = p*l*t; p = 8, l=4, t=3 ) adalah ".$panjang*$lebar*$tinggi."<br><br>";
+
+    //2. Volume Kubus
+    $r = 10;
+    echo "Volume kubus ( V = r*r*r; r = 10 ) adalah ".$r*$r*$r."<br><br>";
+
+    //3. Volume Bola
+    $phi = 3.14;
+    echo "Volume Bola ( V = 4/3*phi*r*r*r; phi = 3.14, r = 10) adalah ".(4/3*$phi*$r*$r*$r)."<br><br>";
+?>
